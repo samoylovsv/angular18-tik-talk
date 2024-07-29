e6ce03da2895dfb9718a02d94c44bc9455bb3e83
samoylovsv <samoylovsv@gmail.com>
2024-07-29 11:16:25 +0300

    тест впн

---
+++ tik-talk/src/app/data/services/profile.service.ts
@@ -0,0 +1,21 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Profile} from "../interfaces/profile.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  http: HttpClient = inject(HttpClient)
+
+  baseApiUrl: string = 'https://icherniakov.ru/yt-course/'
+
+  constructor() { }
+
+  gerTestAccounts(): Observable<Profile[]> {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`)
+  }
+
+}

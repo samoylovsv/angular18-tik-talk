fce1a602c58d3e9ddf806c070232e3f53b4fea65
samoylovsv <samoylovsv@gmail.com>
2024-07-31 09:22:22 +0300

    layout routs

--- tik-talk/src/app/app.component.ts
+++ tik-talk/src/app/app.component.ts
@@ -1,24 +1,13 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ProfileCardComponent} from "./common-ui/profile-card/profile-card.component";
-import {ProfileService} from "./data/services/profile.service";
-import {Profile} from "./data/interfaces/profile.interface";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  profileService = inject(ProfileService)
-  profiles: Profile[] = []
 
-  constructor() {
-    this.profileService.gerTestAccounts()
-      .subscribe(value => {
-        this.profiles = value
-      })
-  }
 }
---
+++ tik-talk/src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,23 @@
+import {Component, inject} from '@angular/core';
+import {ProfileService} from "../../data/services/profile.service";
+import {Profile} from "../../data/interfaces/profile.interface";
+import {ProfileCardComponent} from "../../common-ui/profile-card/profile-card.component";
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [ProfileCardComponent],
+  templateUrl: './search-page.component.html',
+  styleUrl: './search-page.component.scss'
+})
+export class SearchPageComponent {
+  profileService = inject(ProfileService)
+  profiles: Profile[] = []
+
+  constructor() {
+    this.profileService.gerTestAccounts()
+      .subscribe(value => {
+        this.profiles = value
+      })
+  }
+}

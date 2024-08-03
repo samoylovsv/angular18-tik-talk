ff89ebb7724deb0191e772482560dd07717a9c48
samoylovsv <samoylovsv@gmail.com>
2024-08-03 07:57:18 +0300

    menuItems

--- tik-talk/src/app/common-ui/sidebar/sidebar.component.ts
+++ tik-talk/src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import {SvgIconComponent} from "../svg-icon/svg-icon.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    SvgIconComponent
+    SvgIconComponent,
+    NgForOf
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chat',
+      link: 'chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    }
+  ]
 
 }
